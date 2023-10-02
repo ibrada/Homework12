c37e18ed813f2b5ba7574d4765233655eb8140a3
ibrada <radikibragimov@mail.ru>
2023-10-03 01:29:57 +0300

    hw13 fixed, final release (hw12 too) I did it :D

--- src/Homework13/Homework13.java
+++ src/Homework13/Homework13.java
@@ -9,7 +9,7 @@ public class Homework13 {
         System.out.println(verber.getFirstname() + " " + verber.getSurname());
 
         Book book1 = new Book("1984", oruell, 1949);
-        Book book2 = new Book("Шестой сон", oruell, 2015);
+        Book book2 = new Book("Шестой сон", verber, 2015);
         System.out.println(book1.getTitle() + " " + book1.getAuthor() + " " + book1.getYear());
         System.out.println(book2.getTitle() + " " + book2.getAuthor() + " " + book2.getYear());
 
